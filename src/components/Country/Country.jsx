45a608dcab5a5f93265a5672964f707173eeb2d7
Abadat Hossain <abadat26@gmail.com>
2025-03-11 12:57:09 +0600

    conditional css done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -4,17 +4,18 @@ const Country = ({ country }) => {
     console.log(country)
     const { name, capital, flags, population, region } = country;
     const [visited, setVisited] = useState(false);
+
     const handleVisited = () => {
         setVisited(!visited);
     }
     return (
-        <div className="country-container">
+        <div className={`country-container ${visited && 'visit-country'}`}>
             <h1>{name.common}</h1>
             <h3>{capital}</h3>
             <img src={flags.png} alt="" />
             <h3>{population}</h3>
             <h3>{region}</h3>
-            <button onClick={handleVisited}>Visited</button>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
             {/* {
                 visited && <p style={{ color: 'green' }}>You have visited {name.common}</p>
             } */}
